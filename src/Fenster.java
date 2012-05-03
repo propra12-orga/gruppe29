7e8c8b1be3e7b6368b2af98d6fd581b65640f6ae
Moritz Kanzler <moritzkanzler@googlemail.com>
2012-05-03 13:27:43 +0200

    Mauer added

--- src/Fenster.java
+++ src/Fenster.java
@@ -1,4 +1,6 @@
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Fenster {
 
@@ -6,8 +8,12 @@ public class Fenster {
 		JFrame f = new JFrame("Bomberman");
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setSize(600, 600);
-		f.add(new Mauer(10, 10));
-		f.add(new Mauer(200, 50));
+		JPanel p = new JPanel();
+		f.setContentPane(p);
+		p.setLayout(new BoxLayout(p, BoxLayout.PAGE_AXIS));
+		p.add(new Mauer(10, 10));
+		p.add(new Mauer(200, 50));
+		p.add(new Mauer(1, 200));
 		f.setVisible(true);
 	}
 }
