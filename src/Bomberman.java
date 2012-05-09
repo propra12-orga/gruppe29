b8c68b348f92bcc196915c2b29bd6cf8347f192d
Moritz Kanzler <moritzkanzler@googlemail.com>
2012-05-09 13:05:45 +0200

    README remove

--- src/Bomberman.java
+++ src/Bomberman.java
@@ -3,34 +3,45 @@ import java.awt.Graphics;
 
 public class Bomberman {
 
-	int posX;
-	int posY;
+	private int posX;
+	private int posY;
+	private int radius;
+	private int steps;
 
-	public Bomberman(int posX, int posY) {
-		super();
+	public Bomberman(int posX, int posY, int radius) {
 		this.posX = posX;
 		this.posY = posY;
+		this.radius = radius;
+		setSteps(radius);
 	}
 
 	public void paintBomberman(Graphics g) {
 		g.setColor(Color.red);
-		g.fillOval(this.posX, this.posY, 10, 10);
+		g.fillOval(this.posX, this.posY, radius, radius);
 	}
 
 	public void moveUp() {
-		setPosY(getPosY() - 10);
+		setPosY(getPosY() - steps);
 	}
 
 	public void moveDown() {
-		setPosY(getPosY() + 10);
+		setPosY(getPosY() + steps);
 	}
 
 	public void moveLeft() {
-		setPosX(getPosX() - 10);
+		setPosX(getPosX() - steps);
 	}
 
 	public void moveRight() {
-		setPosX(getPosX() + 10);
+		setPosX(getPosX() + steps);
+	}
+
+	public int getRadius() {
+		return radius;
+	}
+
+	public void setRadius(int radius) {
+		this.radius = radius;
 	}
 
 	public int getPosX() {
@@ -47,6 +58,14 @@ public class Bomberman {
 
 	public void setPosY(int posY) {
 		this.posY = posY;
+	}
+
+	public int getSteps() {
+		return steps;
+	}
+
+	public void setSteps(int steps) {
+		this.steps = steps;
 	}
 
 }
--- src/Spielfeld.java
+++ src/Spielfeld.java
@@ -8,83 +8,120 @@ public class Spielfeld extends JPanel implements KeyListener {
 
 	@Override
 	public void keyPressed(KeyEvent ke) {
-		if (ke.getKeyCode() == KeyEvent.VK_UP) {
-			System.out.println("UP");
-			this.bm.moveUp();
-			repaint();
-		}
-		if (ke.getKeyCode() == KeyEvent.VK_DOWN) {
-			System.out.println("DOWN");
-			this.bm.moveDown();
-			repaint();
-		}
-		if (ke.getKeyCode() == KeyEvent.VK_LEFT) {
-			System.out.println("LEFT");
-			this.bm.moveLeft();
-			repaint();
-		}
-		if (ke.getKeyCode() == KeyEvent.VK_RIGHT) {
-			System.out.println("RIGHT");
-			this.bm.moveRight();
-			repaint();
+		switch (ke.getKeyCode()) {
+		case KeyEvent.VK_UP:
+			if (this.raster[this.bm.getPosX()][this.bm.getPosY()
+					- this.bm.getSteps()] != true) {
+				this.bm.moveUp();
+				repaint();
+				break;
+			} else
+				break;
+		case KeyEvent.VK_DOWN:
+			if (this.raster[this.bm.getPosX()][this.bm.getPosY()
+					+ this.bm.getSteps()] != true) {
+				this.bm.moveDown();
+				repaint();
+				break;
+			} else
+				break;
+		case KeyEvent.VK_LEFT:
+			if (this.raster[this.bm.getPosX() - this.bm.getSteps()][this.bm
+					.getPosY()] != true) {
+				this.bm.moveLeft();
+				repaint();
+				break;
+			} else
+				break;
+		case KeyEvent.VK_RIGHT:
+			if (this.raster[this.bm.getPosX() + this.bm.getSteps()][this.bm
+					.getPosY()] != true) {
+				this.bm.moveRight();
+				repaint();
+				break;
+			} else
+				break;
+		default:
+			break;
 		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void keyTyped(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-
 	}
 
 	// Groesse des Fensters
 	private int width;
 	private int height;
 
+	private int border; // rand unten&rechts
+	private int field; // laenge des spielfelds
+
+	// Spielraster
+	private boolean[][] raster;
+
 	// Spielfeldobjekte
 	private Bomberman bm;
 
 	// Groesse der Bloecke
-	private final int blockWidth;
-	private final int blockHeight;
+	private int blockLength;
 
 	public Spielfeld(int width, int height) {
 		this.height = height;
 		this.width = width;
-		blockHeight = height / 10;
-		blockWidth = width / 10;
+		findVariables();
+		this.bm = new Bomberman(width - border - field, border, blockLength);
+		this.raster = new boolean[width][height];
+		for (int i = 0; i < width; i++)
+			for (int j = 0; j < height; j++)
+				this.raster[i][j] = false;
+	}
 
-		this.bm = new Bomberman(100, 100);
+	private void findVariables() {
+		int tmp = height;
+		while (tmp % 9 != 0) {
+			tmp -= 10;
+		}
+		field = tmp;
+		blockLength = field / 9;
+		border = (height - field) / 2;
 	}
 
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		// Rand zeichnen
-		for (int i = 0; i < width; i = i + blockWidth / 2) {
-			g.fillRect(i, 0, blockWidth, blockHeight / 2);
-			g.fillRect(i, height - blockHeight / 2, blockWidth, blockHeight / 2);
-		}
-		for (int i = 0; i < height; i = i + blockHeight / 2) {
-			g.fillRect(0, i, blockWidth / 2, blockHeight);
-			g.fillRect(width - blockWidth / 2, i, blockWidth / 2, blockHeight);
-		}
+		g.fillRect(0, 0, width, border);
+		for (int i = 0; i < width; i++)
+			for (int j = 0; j < border; j++)
+				this.raster[i][j] = true;
+		g.fillRect(0, height - border, width, border);
+		for (int i = 0; i < width; i++)
+			for (int j = height - border; j < border; j++)
+				this.raster[i][j] = true;
+		g.fillRect(width - border, border, border, height);
+		for (int i = width - border; i < border; i++)
+			for (int j = border; j < height; j++)
+				this.raster[i][j] = true;
+		g.fillRect(0, border, width - border - field, height - 2 * border);
+		for (int i = 0; i < width - border - field; i++)
+			for (int j = border; j < height - 2 * border; j++)
+				this.raster[i][j] = true;
 
 		// Blöcke zeichnen
-		for (int i = blockWidth + blockWidth / 2; i < width; i = i + 2
-				* blockWidth) {
-			for (int j = blockHeight + blockHeight / 2; j < height; j = j + 2
-					* blockHeight) {
-				g.fillRect(i, j, blockWidth, blockHeight);
+		for (int i = width - border - field + blockLength; i < width - border; i += 2 * blockLength) {
+			for (int j = border + blockLength; j < height - border; j += 2 * blockLength) {
+				g.fillRect(i, j, blockLength, blockLength);
+				for (int k = 0; k < blockLength; k++)
+					for (int l = 0; l < blockLength; l++)
+						this.raster[i + k][j + l] = true;
 			}
 		}
 
 		// Bomberman zeichnen
 		bm.paintBomberman(g);
 	}
-
 }
