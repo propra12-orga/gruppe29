982e3e99beb0539c17aad710139ec6e7629d2706
Sascha Kuhnke <sascha.kuhnke@uni-duesseldorf.de>
2012-06-12 13:27:03 +0200

    endlich

--- src/Bomberman.java
+++ src/Bomberman.java
@@ -6,16 +6,34 @@ import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
 public class Bomberman extends JLabel {
-
+	/**
+	 * posX, posY Koordinaten des Bomberman; radius Pixelgröße der
+	 * Bombermanfigur
+	 */
 	private int posX;
 	private int posY;
 	private int radius;
+	/** Geschwindigkeit */
 	private int steps;
-
+	/** Liste mit allen Bomben des Bombermans */
 	public List<Bomb> bombs;
+	/** Explosionsradius der Bomben */
 	private int expRad;
+	/** Anzahl der Bomben in der Liste */
 	public int counter;
 
+	/**
+	 * Erstellt einen neuen Bomberman und initialisiert die Werte.
+	 * 
+	 * @param posX
+	 *            Startposition
+	 * @param posY
+	 *            Startposition
+	 * @param radius
+	 *            Pixelgröße der Bombermanfigur
+	 * @param expRad
+	 *            Explosionsradius
+	 */
 	public Bomberman(int posX, int posY, int radius, int expRad) {
 		this.posX = posX;
 		this.posY = posY;
@@ -30,6 +48,21 @@ public class Bomberman extends JLabel {
 		setSteps(radius);
 	}
 
+	/**
+	 * Erstellt einen neuen Bomberman und initialisiert die Werte. Möglichkeit
+	 * die Farbe des Bomberman zu ändern.
+	 * 
+	 * @param posX
+	 *            Startposition
+	 * @param posY
+	 *            Startposition
+	 * @param radius
+	 *            Pixelgröße der Bombermanfigur
+	 * @param expRad
+	 *            Explosionsradius
+	 * @param color
+	 *            Wert für Farbe des Bomberman
+	 */
 	public Bomberman(int posX, int posY, int radius, int expRad, int color) {
 		this(posX, posY, radius, expRad);
 		String path_to_Image;
@@ -44,12 +77,20 @@ public class Bomberman extends JLabel {
 		setSteps(radius);
 	}
 
+	/**
+	 * Fordert eine neue Bombe an.
+	 * 
+	 * @return Index auf die neu erstellte Bombe
+	 */
 	public int getNewBombIndex() {
 		Bomb bomb = new Bomb(this.posX, this.posY, this.radius, this.expRad);
 		this.bombs.add(bomb);
 		return this.bombs.lastIndexOf(bomb);
 	}
 
+	/**
+	 * Zeichnen der Bombe
+	 */
 	public void paintObject() {
 		this.setBounds(this.posX, this.posY, this.radius, this.radius);
 		/*
@@ -58,50 +99,102 @@ public class Bomberman extends JLabel {
 		 */
 	}
 
+	/**
+	 * Ändert die Y-Position nach oben um steps.
+	 */
 	public void moveUp() {
 		setPosY(getPosY() - steps);
 	}
 
+	/**
+	 * Ändert die Y-Position nach unten um steps.
+	 */
 	public void moveDown() {
 		setPosY(getPosY() + steps);
 	}
 
+	/**
+	 * Ändert die X-Position nach links um steps.
+	 */
 	public void moveLeft() {
 		setPosX(getPosX() - steps);
 	}
 
+	/**
+	 * Ändert die X-Position nach rechts um steps.
+	 */
 	public void moveRight() {
 		setPosX(getPosX() + steps);
 	}
 
+	/**
+	 * 
+	 * @return Pixelgröße des Bomberman
+	 */
 	public int getRadius() {
 		return radius;
 	}
 
+	/**
+	 * Setzt Pixelgröße des Bomberman
+	 * 
+	 * @param radius
+	 *            Pixelgröße des Bomberman
+	 */
 	public void setRadius(int radius) {
 		this.radius = radius;
 	}
 
+	/**
+	 * 
+	 * @return X-Position des Bomberman
+	 */
 	public int getPosX() {
 		return posX;
 	}
 
+	/**
+	 * Setzt X-Position des Bomberman
+	 * 
+	 * @param posX
+	 *            X-Position des Bomberman
+	 */
 	public void setPosX(int posX) {
 		this.posX = posX;
 	}
 
+	/**
+	 * 
+	 * @return Y-Position des Bomberman
+	 */
 	public int getPosY() {
 		return posY;
 	}
 
+	/**
+	 * Setzt Y-Position des Bomberman
+	 * 
+	 * @param posY
+	 *            Y-Position des Bomberman
+	 */
 	public void setPosY(int posY) {
 		this.posY = posY;
 	}
 
+	/**
+	 * 
+	 * @return Geschwindigkeit des Bomberman
+	 */
 	public int getSteps() {
 		return steps;
 	}
 
+	/**
+	 * Setze Geschwindigkeit des Bomberman
+	 * 
+	 * @param steps
+	 *            Geschwindigkeit des Bomberman
+	 */
 	public void setSteps(int steps) {
 		this.steps = steps;
 	}
--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,53 @@
+import java.io.IOException;
+
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.input.SAXBuilder;
 
 public class Main {
+	/** Pixelraster */
 	public static int[][] raster;
+	/** Blockraster */
 	private static int[][] raster2;
-	private static int sizeX; // Breite des Fensters
-	private static int sizeY; // Hoehe des Fensters
-	private static int length; // Laenge der Bloecke
-	private static int columns; // Anzahl der Reihen und Spalten
-								// (gaenge+mauerreihen+rand)
+	/** sizeX, sizeY Breite und Höhe des Fensters */
+	private static int sizeX;
+	private static int sizeY;
+	/** Länge der Blöcke */
+	private static int length;
+	/** Anzahl der Reihen und Spalten (gaenge+mauerreihen+rand) */
+	private static int columns;
+	/** Fenster für das Spiel */
 	public static Fenster f;
 
+	/**
+	 * main-Methode des Spiels, hier fängt Alles an. Raster werden eingelsen und
+	 * erzeugt, anschließend damit das Fenster erstellt.
+	 * 
+	 * @exception IOException
+	 *                , JDOMException
+	 */
 	public static void main(String[] args) {
-		columns = 13;
+		columns = 9;
 		length = 50;
-		sizeX = columns * length;
-		sizeY = columns * length;
+		sizeX = sizeY = columns * length;
 		raster = new int[sizeX][sizeY];
 		raster2 = new int[columns][columns];
 		try {
 			feldEinlesen();
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		transform();
 		f = new Fenster(sizeX, sizeY, columns, length);
 	}
 
+	/**
+	 * Methode zum Einlesen des Spielfelds aus der XML-Datei "Bomberman.xml". Es
+	 * wird ein blockraster erstellt.
+	 * 
+	 * @throws IOException
+	 *             , JDOMException
+	 */
 	private static void feldEinlesen() throws Exception {
 		String filename = "Bomberman.xml";
 		Document doc = new SAXBuilder().build(filename);
@@ -41,12 +60,16 @@ public class Main {
 			}
 	}
 
+	/**
+	 * Methode zum erstellen des Pixelrasters aus einem Blockraster. (Jede Zelle
+	 * aus dem Blockraster wird zu einem Pixelblock im Pixelraster ausgeweitet).
+	 */
 	private static void transform() {
 		for (int k = 0; k < columns; k++)
 			for (int l = 0; l < columns; l++) {
 				for (int i = 0; i < length; i++)
 					for (int j = 0; j < length; j++)
-						raster[((k * sizeX) / columns) + i][((l * sizeX) / columns)
+						raster[((k * sizeX) / columns) + i][((l * sizeY) / columns)
 								+ j] = raster2[k][l];
 			}
 	}
