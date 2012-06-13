835d32473fbe28ec136b595a50b59d28b4324d66
Sascha Kuhnke <sascha.kuhnke@uni-duesseldorf.de>
2012-06-13 11:54:25 +0200

    Bombe explodiert richtig

--- src/BombExplosion.java
+++ src/BombExplosion.java
@@ -40,6 +40,7 @@ public class BombExplosion extends TimerTask {
 	public void run() {
 		bomb.setExploded(true);
 		System.out.println("Bombe gezündet!");
+		// Bomberman 1 pruefen
 		if ((this.bm.getPosX() >= (this.bomb.getPosX() - this.bomb
 				.getExplosionRadius()))
 				&& (this.bm.getPosX() <= (this.bomb.getPosX() + this.bomb
@@ -50,16 +51,7 @@ public class BombExplosion extends TimerTask {
 						&& (this.bm.getPosY() <= (this.bomb.getPosY() + this.bomb
 								.getExplosionRadius())) && (this.bm.getPosX() == this.bomb
 						.getPosX()))) {
-			// (this.bm.getPosX() + this.bm.getSteps() - 1 ==
-			// this.bomb.getPosX())
-			// || ((this.bm.getPosX() - this.bm.getSteps() + 1 == this.bomb
-			// .getPosX()))
-			// || (this.bm.getPosY() + this.bm.getSteps() - 1 == this.bomb
-			// .getPosY())
-			// || (this.bm.getPosY() - this.bm.getSteps() + 1 == this.bomb
-			// .getPosY())
-			// || ((this.bm.getPosX() == this.bomb.getPosX()) && (this.bm
-			// .getPosY() == this.bomb.getPosY())))
+			this.sp.repaint();
 			Main.f.dispose(
 					"Deine eigene Bombe ist explodiert und hat dich mitgerissen!",
 					true);
@@ -68,6 +60,7 @@ public class BombExplosion extends TimerTask {
 			else
 				Main.f.restart(1);
 		}
+		// Bomberman 2 pruefen
 		if (this.bm2 != null) {
 			if ((this.bm2.getPosX() >= (this.bomb.getPosX() - this.bomb
 					.getExplosionRadius()))
@@ -79,22 +72,13 @@ public class BombExplosion extends TimerTask {
 							&& (this.bm2.getPosY() <= (this.bomb.getPosY() + this.bomb
 									.getExplosionRadius())) && (this.bm2
 							.getPosX() == this.bomb.getPosX()))) {
-				// (this.bm2.getPosX() + this.bm2.getSteps() - 1 ==
-				// this.bomb.getPosX())
-				// || ((this.bm2.getPosX() - this.bm2.getSteps() + 1 ==
-				// this.bomb
-				// .getPosX()))
-				// || (this.bm2.getPosY() + this.bm2.getSteps() - 1 == this.bomb
-				// .getPosY())
-				// || (this.bm2.getPosY() - this.bm2.getSteps() + 1 == this.bomb
-				// .getPosY())
-				// || ((this.bm2.getPosX() == this.bomb.getPosX()) && (this.bm2
-				// .getPosY() == this.bomb.getPosY())))
+				this.sp.repaint();
 				Main.f.dispose("Dein Gegenspieler hat dich erledigt!", true);
 				Main.f.restart(2);
 			}
 		}
-		// Zerstörung der Mauern
+
+		// Zerstoerung der Mauern
 		// oben&unten
 		boolean flag = false;
 		int swap = -1;
@@ -108,13 +92,13 @@ public class BombExplosion extends TimerTask {
 
 			if (this.sp.raster[this.bomb.getPosX()][i] == 1) {
 				flag = true;
-				break; // unzerstörbare mauer stoppt explosion
+				break; // unzerstoerbare mauer stoppt explosion
 			} else if (this.sp.raster[this.bomb.getPosX()][i] == 2) {
-				// zerstörbare mauer
+				// zerstoerbare mauer
 				swap = i;
 				System.out.println("Hallo");
 
-			} // wird zerstört
+			} // wird zerstoert
 			else if (this.sp.raster[this.bomb.getPosX()][i] == 3) {
 				// andere bombe muss explodieren
 			} else if (this.sp.raster[this.bomb.getPosX()][i] == 4) {
@@ -139,13 +123,13 @@ public class BombExplosion extends TimerTask {
 
 			if (this.sp.raster[i][this.bomb.getPosY()] == 1) {
 				flag = true;
-				break; // unzerstörbare mauer stoppt explosion
+				break; // unzerstoerbare mauer stoppt explosion
 			} else if (this.sp.raster[i][this.bomb.getPosY()] == 2) {
-				// zerstörbare mauer
+				// zerstoerbare mauer
 				swap = i;
 				System.out.println("Hallo");
 
-			} // wird zerstört
+			} // wird zerstoert
 			else if (this.sp.raster[i][this.bomb.getPosY()] == 3) {
 				// andere bombe muss explodieren
 			} else if (this.sp.raster[i][this.bomb.getPosY()] == 3) {
@@ -158,63 +142,5 @@ public class BombExplosion extends TimerTask {
 					this.sp.raster[swap + k][this.bomb.getPosY() + l] = 0;
 
 		this.sp.repaint();
-
-		// if (this.sp.raster[this.bomb.getPosX() -
-		// this.bomb.getRadius()][this.bomb
-		// .getPosY()] == 2) {
-		// int x = this.bomb.getPosX() - this.bomb.getRadius();
-		// int y = this.bomb.getPosY();
-		// while (this.sp.raster[(x - 1)][this.bomb.getPosY()] == 2)
-		// x -= 1;
-		// while (this.sp.raster[this.bomb.getPosX() - this.bomb.getRadius()][y
-		// - 1] == 2)
-		// y -= 1;
-		// for (int i = 0; i < this.sp.blockLength; i++)
-		// for (int j = 0; i < this.sp.blockLength; j++)
-		// this.sp.raster[x + i][y + j] = 0;
-		// }
-		// // nach rechts
-		// else if (this.sp.raster[this.bomb.getPosX() +
-		// this.bomb.getRadius()][this.bomb
-		// .getPosY()] == 2) {
-		// int x = this.bomb.getPosX() + this.bomb.getRadius();
-		// int y = this.bomb.getPosY();
-		// while (this.sp.raster[(x - 1)][this.bomb.getPosY()] == 2)
-		// x -= 1;
-		// while (this.sp.raster[this.bomb.getPosX() + this.bomb.getRadius()][y
-		// - 1] == 2)
-		// y -= 1;
-		// for (int i = 0; i < this.sp.blockLength; i++)
-		// for (int j = 0; i < this.sp.blockLength; j++)
-		// this.sp.raster[x + i][y + j] = 0;
-		// }
-		// // nach oben
-		// else if (this.sp.raster[this.bomb.getPosX()][this.bomb.getPosY()
-		// - this.bomb.getRadius()] == 2) {
-		// int x = this.bomb.getPosX();
-		// int y = this.bomb.getPosY() - this.bomb.getRadius();
-		// while (this.sp.raster[(x - 1)][this.bomb.getPosY()
-		// - this.bomb.getRadius()] == 2)
-		// x -= 1;
-		// while (this.sp.raster[this.bomb.getPosX()][y - 1] == 2)
-		// y -= 1;
-		// for (int i = 0; i < this.sp.blockLength; i++)
-		// for (int j = 0; i < this.sp.blockLength; j++)
-		// this.sp.raster[x + i][y + j] = 0;
-		// }
-		// // nach unten
-		// else if (this.sp.raster[this.bomb.getPosX()][this.bomb.getPosY()
-		// + this.bomb.getRadius()] == 2) {
-		// int x = this.bomb.getPosX();
-		// int y = this.bomb.getPosY() + this.bomb.getRadius();
-		// while (this.sp.raster[(x - 1)][this.bomb.getPosY()
-		// + this.bomb.getRadius()] == 2)
-		// x -= 1;
-		// while (this.sp.raster[this.bomb.getPosX()][y - 1] == 2)
-		// y -= 1;
-		// for (int i = 0; i < this.sp.blockLength; i++)
-		// for (int j = 0; i < this.sp.blockLength; j++)
-		// this.sp.raster[x + i][y + j] = 0;
-		// }
 	}
 }
