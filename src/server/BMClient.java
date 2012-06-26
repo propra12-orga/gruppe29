269b79b3be3eb77b45b2e02af1bab12eb5f56bd4
Tim Fischer <Tim.Fischer@uni-duesseldorf.de>
2012-06-26 14:24:23 +0200

    client

--- src/server/BMClient.java
+++ src/server/BMClient.java
@@ -1,22 +1,57 @@
 package server;
 
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.net.Socket;
 
 public class BMClient {
 
 	public static void main(String[] args) {
 		Socket server = null;
+		String xml = "";
+		byte character;
+		File f = new File("maps/Default.xml");
 
 		try {
-			server = new Socket("localhost", 3141);
-			InputStream in = server.getInputStream();
-			OutputStream out = server.getOutputStream();
+			System.out.println("Datei lesen...");
+			FileInputStream fis = new FileInputStream(f);
 
-			out.write(5);
-			int result = in.read();
-			System.out.println(result);
+			do {
+				character = (byte) fis.read();
+				System.out.println("CHAR: " + (char) character);
+				if (character != -1)
+					xml += (char) character;
+			} while (character != -1);
+
+			fis.close();
+
+			System.out.println("Datei gelesen");
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		try {
+			server = new Socket("134.99.38.235", 3143);
+			DataInputStream in = new DataInputStream(server.getInputStream());
+			DataOutputStream out = new DataOutputStream(
+					server.getOutputStream());
+
+			out.writeUTF(xml);
+			System.out.println("Abgabe an Server");
+			String xmlin = in.readUTF();
+
+			// VOM SERVER LESEN UND SPEICHERN
+			FileOutputStream fos = new FileOutputStream("maps/temp.xml");
+			for (int i = 0; i < xml.length(); i++) {
+				fos.write((byte) xmlin.charAt(i));
+			}
+			fos.close();
+			System.out.println("Datei gespeichert");
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
